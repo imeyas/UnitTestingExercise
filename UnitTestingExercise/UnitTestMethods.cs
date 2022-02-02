d70215dff0404039be8a0f21a745c3a67439b6e5
Ebo Onwunali <ebo@njgeneral.com>
2022-02-02 11:02:21 -0500

    First commit - finished methods creation

--- UnitTestingExercise/UnitTestMethods.cs
+++ UnitTestingExercise/UnitTestMethods.cs
@@ -4,14 +4,43 @@ namespace UnitTestingExercise
     public class UnitTestMethods
     {
         // Make sure your references are set properly
-            // Righ-click on Dependencies in your .Tests project and set your reference
+        // Righ-click on Dependencies in your .Tests project and set your reference
 
 
+        public static int AddMethod(int num1, int num2, int num3)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int SubtractMethod(int minuend, int subtrahend)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int MultiplyMethod(int multnum1, int multnum2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static int DivideMethod(int dividend, int divisor)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void UtilizeFactTest1()
+        {
+
+        }
+
+        public static void UtilizeFactTest2()
+        {
+
+        }
 
         // Create an Add method that passes 3 integers
 
         // Create a Subtract method that passes 2 integers
-            // Keep track of which number is getting passed as minuend and subtrahend
+        // Keep track of which number is getting passed as minuend and subtrahend
 
         // Create a Multiply method that passes 2 integers
 
@@ -21,6 +50,6 @@ namespace UnitTestingExercise
 
         // Create 2 methods that will utilize the [Fact] tests you wrote
 
-        
+
     }
 }
